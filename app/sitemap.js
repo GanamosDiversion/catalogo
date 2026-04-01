e79d71fa408bc730a888b4ca2642759474d5bf01
GanamosDiversion <mariano.ambort.2014@gmail.com>
2026-04-01 03:36:39 -0500

    fix: update favicon to icono.png

---
+++ app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.ganamosdiversion.com",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://www.ganamosdiversion.com/terminos",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+  ]
+}
